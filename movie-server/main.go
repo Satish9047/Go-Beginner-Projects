d8e2d466b745b2b5bc9da6b06800ec9b829171a0
Satish9047 <satishprajapati930@gmail.com>
2024-05-30 15:37:07 +0545

    5: add delete movie functionality

--- movie-server/main.go
+++ movie-server/main.go
@@ -9,17 +9,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// import (
-// 	"encoding/json"
-// 	"fmt"
-// 	"log"
-// 	"math/rand"
-// 	"net/http"
-// 	"strconv"
-// 	"github.com/gorilla/mux"
-// )
-
-
 type Movie struct {
 	ID string `json:"id"`
 	Isbn string `json:"isbn"`
@@ -39,10 +28,23 @@ func getMovies(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(movies)
 }
-func getMovie(w http.ResponseWriter, r *http.Request){}
+
+//get
+func getMovie(w http.ResponseWriter, r *http.Request) {}
 func createMovie(w http.ResponseWriter, r *http.Request){}
 func updateMovie(w http.ResponseWriter, r *http.Request){}
-func deleteMovie(w http.ResponseWriter, r *http.Request){}
+func deleteMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params:= mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"]{
+			movies =  append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+}
+
+
 
 
 func main (){
